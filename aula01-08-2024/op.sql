83e9eae46cb52ab4c803c4332a962dd57aaf4985
Adrian Feijó Fagundes <adrianfeijofagundes@gmail.com>
2024-08-01 21:51:02 -0300

    operadores e subconsultas

---
+++ aula01-08-2024/op.sql
@@ -0,0 +1,14 @@
+-- ON
+
+SELECT nome, departamento_id
+FROM empregados
+WHERE departamento_id IN (valor1, valor2, valor3)
+
+
+BETWEEN 3000 AND 5000 -- entre
+
+WHERE nome LIKE 'K%';
+WHERE nome LIKE '%an%';
+WHERE nome LIKE 'A___';
+
+ANY
